384b2107c905a9619e59825e452b3fea5399b530
TheCmar7 <smithcr@flip3.engr.oregonstate.edu>
2015-07-23 13:10:58 -0700

    I made the thing work, it was yesterday so I forgot most of it, but I will try to sum it up for you.     login goes to logout     logout checks the username     logout log out creates the session variables     logout then takes you to content 1     content1 checks the session stuff     then the user can either a) logout or b)go to content2         a)logout then destoys the session and takes you to logout         b)content2 is the same as content1

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php 	
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>LOG IN</title>
+	</head>
+
+	<body>
+		<h1>LOG IN</h1>
+		<form action="http://web.engr.oregonstate.edu/~smithcr/assignment5/logout.php" method="POST">
+			User Name: <input type="text" name="userName">
+			<input type="submit" value="Log in">
+		</form>
+	</body>
+</html>
+	
