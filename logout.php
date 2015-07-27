5d732ed50f7e5e8e522116d2863d5144ee151a70
TheCmar7 <smithcr@flip1.engr.oregonstate.edu>
2015-07-26 20:25:47 -0700

    I made it so it was not the log out and the log in page

--- logout.php
+++ logout.php
@@ -3,11 +3,6 @@
 	if(isset($_SESSION["user"])) {
 		session_destroy();
 		header("Location: http://web.engr.oregonstate.edu/~smithcr/assignment5/login.php");
-	} else if (isset($_POST["userName"])) {
-		$_SESSION["user"] = $_POST["userName"];
-		$_SESSION["count"] = "0";
-		header("Location: http://web.engr.oregonstate.edu/~smithcr/assignment5/content1.php");
-	} else { 
-		header("Location: http://web.engr.oregonstate.edu/~smithcr/assignment5/login.php");
 	}
+	header("Location: http://web.engr.oregonstate.edu/~smithcr/assignment5/login.php");
 ?>
