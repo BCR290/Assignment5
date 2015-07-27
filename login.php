ae54286ef37938284491742f247dc22d8f2d64be
TheCmar7 <smithcr@flip1.engr.oregonstate.edu>
2015-07-26 20:27:10 -0700

    I don't know what I changed in this file... `:wq

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
 
 	<body>
 		<h1>LOG IN</h1>
-		<form action="http://web.engr.oregonstate.edu/~smithcr/assignment5/logout.php" method="POST">
+		<form action="http://web.engr.oregonstate.edu/~smithcr/assignment5/content1.php" method="POST">
 			User Name: <input type="text" name="userName">
 			<input type="submit" value="Log in">
 		</form>
